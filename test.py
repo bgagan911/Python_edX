bc2234f6ad6eb41bac793ab7e16239d5aa8149c7
Gagan Bhalla <bgagan911@live.in>
2019-05-01 23:33:43 +0530

    Checking first file.

---
+++ test.py
@@ -0,0 +1,15 @@
+# [ ] review and run example
+student_name = "Joana"
+
+# get last letter
+end_letter = student_name[-1]
+print(student_name,"ends with", "'" + end_letter + "'")
+# [ ] review and run example
+# get second to last letter
+second_last_letter = student_name[-2]
+print(student_name,"has 2nd to last letter of", "'" + second_last_letter + "'")
+# [ ] review and run example
+# you can get to the same letter with index counting + or -
+print("for", student_name)
+print("index 3 =", "'" + student_name[3] + "'")
+print("index -2 =","'" + student_name[-2] + "'")
