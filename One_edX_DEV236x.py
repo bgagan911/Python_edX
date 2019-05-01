b7c8f51d1279f436fa21b993dc0da22cc9368f80
Gagan Bhalla <bgagan911@live.in>
2019-05-02 02:34:45 +0530

    Revision

---
+++ One_edX_DEV236x.py
@@ -0,0 +1,14 @@
+#   This file contains the code for Excercises in the Course DEV236x on edX.
+#   Author  :   Gagan Bhalla
+
+#   --- --- --- --- ---
+msg = "Gagan"
+print("Hello World")
+print('{:>8}'.format(msg),end=' ')
+print("Hello World What are you doing?",...,"-",":)")
+
+#   --- --- --- --- ---
+#   Module 1, 
+new_msg = 'My favorite food is'
+print(new_msg + input("What is your Favorite Food? : "))
+# print(new_msg + userInput)
